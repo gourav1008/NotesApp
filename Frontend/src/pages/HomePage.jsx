a22b2b89edf4310aeb56310bd3bb94fda3421eb1
gourav1008 <gourav8318@gmail.com>
2025-09-14 23:34:58 +0530

    done

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
-import RateLimiterUI from '../components/RateLimiterUI';
+import RateLimiterUi from '../components/RateLimiterUi';
 import toast from 'react-hot-toast'
 import NoteCard from '../components/NoteCard';
 import api from '../lib/axios'
@@ -36,7 +36,7 @@ const HomePage = () => {
   return (
     <div className='min-h-screen'>
       <Navbar />
-      {isRateLimited && <RateLimiterUI /> }
+      {isRateLimited && <RateLimiterUi /> }
       <div className="max-w-7xl mx-auto p-4 mt-6">
           {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
           {notes.length === 0 && !isRateLimited && <NoteNoteFound/>}
